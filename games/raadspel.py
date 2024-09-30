0c53ab2becb09b56cac6783a927e2759a61baca9
Alberto van Eckeveld <67636045+AlbertovanEckeveld@users.noreply.github.com>
2024-09-30 22:41:35 +0200

    Added dagboek.py

--- games/raadspel.py
+++ games/raadspel.py
@@ -1,8 +1,3 @@
-import random
-
-# Vragen aan de gebruiker met welke moeilijkheidsgraad hij het graag wild spelen.
-difficulty = input("Kies de moeilijkheidsgraad: (makkelijk, gemiddeld, moeilijk) ")
-
 """
 Een functie die de gebruiker een getal laat raden, 
 tot de gebruiker het heeft geraden of de pogingen voorbij zijn.
@@ -15,9 +10,73 @@ Argumenten:
     - difficulty (str) makkelijk, gemiddeld of moeilijk
 """
 
+import customtkinter as ctk
+import random
+
+def start_game(difficulty):
+    getal = get_Getal(difficulty)
+    app_spel2 = create_window(f"Raadspel {difficulty}")
+    raadspel_label2 = ctk.CTkLabel(app_spel2, text=f"Raadspel niveau: {difficulty}")
+    raadspel_label2.pack(pady=2)
+
+    app_spel2.lift()
+    app_spel2.mainloop()
+
+
+def get_Getal(difficulty):
+    # moeilijkheidsgraad defineren
+    if difficulty == "makkelijk":
+        pogingen = 3
+        getalstart = 1
+        getalstop = 11
+    elif difficulty == "gemiddeld":
+        pogingen = 5
+        getalstart = 1
+        getalstop = 26
+    elif difficulty == "moeilijk":
+        pogingen = 10
+        getalstart = 1
+        getalstop = 101
+
+    # Een random getal kiezen, geacht de moeilijkheidsgraad.
+    getal = random.randrange(getalstart, getalstop)
+
+    return getal
+
+
+def create_window(title):
+    window = ctk.CTk()
+    window.geometry("800x400")
+    window.title(title)
+    return window
+
+
+def start_button_callback():
+    app_spel = create_window("Raadspel")
+    raadspel_label = ctk.CTkLabel(app_spel, text="Kies de moeilijkheidsgraad")
+    moeilijkheid_makkelijk_button = ctk.CTkButton(app_spel, text="Makkelijk", command=start_game("makkelijk"))
+    moeilijkheid_gemiddeld_button = ctk.CTkButton(app_spel, text="Gemiddeld", command=start_game("gemiddeld"))
+    moeilijkheid_moeilijk_button = ctk.CTkButton(app_spel, text="Moeilijk", command=start_game("moeilijk"))
+    raadspel_label.pack(pady=2)
+    moeilijkheid_makkelijk_button.pack(pady=3)
+    moeilijkheid_gemiddeld_button.pack(pady=3)
+    moeilijkheid_moeilijk_button.pack(pady=3)
+
+    app_spel.lift()
+    app_spel.mainloop()
+
+
+application = create_window("Raadspel")
+
+raadspel_button = ctk.CTkButton(application, text="Klik hier om te starten", command=start_button_callback)
+raadspel_button.pack(padx=40, pady=40)
+
+application.mainloop()
+
+"""
+difficulty = input("Kies de moeilijkheidsgraad: (makkelijk, gemiddeld, moeilijk) ")
+
 def raden(difficulty):
-
-
 
     # for-loop om de pogingen bij te houden
     for i in range(pogingen):
@@ -33,3 +92,5 @@ def raden(difficulty):
 
 # Hier wordt de functie raden aangeroepen om het spel te starten.
 raden(difficulty)
+
+"""
--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import customtkinter
 def button_callback():
     print("button clicked")
 
-app = customtkinter.CTk()
-app.geometry("800x400")
+def main():
+    app = customtkinter.CTk()
+    app.geometry("800x400")
 
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
-customtkinter.set_appearance_mode("light")
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+    customtkinter.set_appearance_mode("light")
 
+    raadspel_button = customtkinter.CTkButton(app, text="Raadspel", command=button_callback)
+    raadspel_button.pack(padx=20, pady=20)
 
+    galgje_button = customtkinter.CTkButton(app, text="Galgje", command=button_callback)
+    galgje_button.pack(padx=20, pady=20)
 
-raadspel_button = customtkinter.CTkButton(app, text="Raadspel", command=button_callback)
-raadspel_button.pack(padx=20, pady=20)
+    dagboek_button = customtkinter.CTkButton(app, text="Dagboek", command=button_callback)
+    dagboek_button.pack(padx=20, pady=20)
 
-galgje_button = customtkinter.CTkButton(app, text="Galgje", command=button_callback)
-galgje_button.pack(padx=20, pady=20)
+    app.mainloop()
 
-dagboek_button = customtkinter.CTkButton(app, text="Dagboek", command=button_callback)
-dagboek_button.pack(padx=20, pady=20)
-
-app.mainloop()
+if __name__ == "__main__":
+    main()
